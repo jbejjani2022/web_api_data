77e1afbc962dffc58612a60f5bb47e776bdb5ed8
jbejjani2022 <jpbejjani@hotmail.com>
2020-05-14 21:20:15 -0400

    Added more customization to pygal graph

--- python_repos.py
+++ python_repos.py
@@ -15,7 +15,7 @@ print("Total repositories:", response_dict['total_count'])
 repo_dicts = response_dict['items']
 print("Repositories returned:", len(repo_dicts))
 
-names, stars = []
+names, stars = [], []
 
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
@@ -23,7 +23,18 @@ for repo_dict in repo_dicts:
 
 # Make visualization.
 my_style = LS('#333366', base_style=LCS)
-chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+my_style.title_font_size = 24
+my_style.label_font_size = 14
+my_style.major_label_font_size = 14
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
