fd8566ced0d7b9650dbb1eec113e7489d191d437
jbejjani2022 <jpbejjani@hotmail.com>
2020-06-10 16:27:58 -0400

    Check status code of API

--- github/python_repos.py
+++ github/python_repos.py
@@ -5,6 +5,7 @@ from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 # Make an API call and store the response.
 url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
 r = requests.get(url)
+# Check status code.
 print("Status code:", r.status_code)
 
 # Store API response in a variable.
